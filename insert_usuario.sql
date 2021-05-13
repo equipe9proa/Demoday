3eee6b654f425b8e4613ce123c48d23ed2fad448
equipe9proa <83553101+equipe9proa@users.noreply.github.com>
2021-05-13 14:45:36 -0300

    Update insert_usuario.sql

--- insert_usuario.sql
+++ insert_usuario.sql
@@ -1,15 +1,14 @@
 -- --
-
-INSERT INTO `usuario` (`ID_usuario`, `nome`, `nome_social`, `e_mail`, `senha`, `cpf`, `data_nasc`, `tel_celular`, `tel_fixo`, `foto`, `end_cep`, `end_estado`, `end_cidade`, `end_bairro`, `end_rua`, `end_numero`, `end_complemento`) VALUES
-(1, 'Victoria de Carvalho Dantas', 'Victoria', 'victoriadecdantas@gmail.com', '12032001Vic@', NULL, '2001-03-12', '11985122172', '41498459', '', '', '', 'R.Barueri', 'Jd Dom José', 'R.Barueri', '34', 's'),
-(2, 'Renan Godoi Pereira Scola', 'Renanzinho da paraiba', 'renanscola@gmail.com', 'naoteinteressa1234', '48738294789', '2002-11-06', '11981647706', '4002-8922', '', '08297658', 'São Paulo', 'São Paulo', 'Itaquera', 'dos alfeneiros', '4', '0'),
-(3, 'Renan Sacola', '', 'contanto.renanscola@gmail.com', 'pédepano2002', '40028922', '2002-11-06', '1170707070', '30265029', '', '08543269', 'Rio Grande do Sul', 'Porto Alegre', 'Jardim Ypu', 'rua do olandes', '25434', ''),
-(4, 'Wagner Moura\r\n', 'o tal do Moura', 'wagnermoura@gmail.com', 'saciperere2001', '4839928339', '2021-05-12', '3083219838', '40028922', '', '3434343', 'Piaui', 'cidade bela', 'bairro nóbrega', 'rua josé binifácio', '23', ''),
-(5, 'Gisele Silvestre', '', 'testegisele@gmail.com', 'chocolate9', '99988877765', '2003-04-18', '(11)959995798', '', '', '08970654', 'São Paulo', 'São Paulo', 'Santiago', 'Rua Flor da Noite', '84', 'A'),
-(6, 'Beatriz Lima', '', 'testebia@gmail.com', 'morango10', '22233344456', '2006-05-09', '(11)933333333', '', '', '04509806', 'São Paulo', 'São Paulo', 'Jardim Robru', 'Rua Mar e Sol', '65', 'B'),
-(7, 'Vinicius Vechi', '', 'testevini@gmail.com', 'torta789', '66677788854', '2002-05-02', '(11)986534587', '(11)27894513', '', '78942601', 'São Paulo', 'São Paulo', 'Guaianases', 'Rua Marechal Tito', '78', 'A'),
-(8, 'Débora Lins', '', 'testedebora@gmail.com', 'laranja18', '18745038936', '2002-07-18', '(11)956327901', '', '', '07506542', 'São Paulo', 'São Paulo', 'Guaianases', 'Rua Nazaré', '67', ''),
-(9, 'Rosana Almeida', 'Rosana', 'testerosana@gmail.com', 'margarida', '35926782691', '1997-06-20', '(11)965321765', '', '', '78954637', 'São Paulo', 'São Paulo', 'Itaim Paulista', 'Rua Morcego cego', '34', 'C'),
-(10, 'Rafael Santana', 'Rafa', 'testerafa@gmail.com', 'jujuba', '26754983672', '1990-03-17', '(11)967543861', '', '', '07563458', 'São Paulo', 'São Paulo', 'Jardim Lua', 'Rua João Barreto', '77', '');
+INSERT INTO `usuario` (`id_usuario`, `nome`, `nome_social`, `e_mail`, `senha`, `cpf`, `data_nasc`, `tel_celular`, `tel_fixo`, `foto`, `end_cep`, `end_estado`, `end_cidade`, `end_bairro`, `end_rua`, `end_numero`, `end_complemento`) VALUES
+(1, 'Tom Cruise Credo', 'James Bonde', 'tomcuise@gmail.com', 'gatoamarelo79', '3483u9', '2011-05-18', '7676575750', '8997865433', '', '09876542', 'São Paulo', 'São Paulo', 'Paraisópolis', 'rua das ruas ', 7, NULL),
+(2, 'Renan Godoi Pereira Scola', 'Renanzinho da paraiba', 'renanscola@gmail.com', 'naoteinteressa1234', '48738294789', '2002-11-06', '11981647706', '4002-8922', '', '08297658', 'São Paulo', 'São Paulo', 'Itaquera', 'dos alfeneiros', 4, 0),
+(3, 'Renan Sacola', '', 'contanto.renanscola@gmail.com', 'pédepano2002', '40028922', '2002-11-06', '1170707070', '30265029', '', '08543269', 'Rio Grande do Sul', 'Porto Alegre', 'Jardim Ypu', 'rua do olandes', 25434, NULL),
+(4, 'Wagner Moura\r\n', 'o tal do Moura', 'wagnermoura@gmail.com', 'saciperere2001', '4839928339', '2021-05-12', '3083219838', '40028922', '', '3434343', 'Piaui', 'cidade bela', 'bairro nóbrega', 'rua josé binifácio', 23, NULL),
+(5, 'Gisele Silvestre', NULL, 'testegisele@gmail.com', 'chocolate9', '99988877765', '2003-04-18', '(11)959995798', NULL, NULL, '08970654', 'São Paulo', 'São Paulo', 'Santiago', 'Rua Flor da Noite', 84, 'A'),
+(6, 'Beatriz Lima', NULL, 'testebia@gmail.com', 'morango10', '22233344456', '2006-05-09', '(11)933333333', NULL, NULL, '04509806', 'São Paulo', 'São Paulo', 'Jardim Robru', 'Rua Mar e Sol', 65, 'B'),
+(7, 'Vinicius Vechi', NULL, 'testevini@gmail.com', 'torta789', '66677788854', '2002-05-02', '(11)986534587', '(11)27894513', NULL, '78942601', 'São Paulo', 'São Paulo', 'Guaianases', 'Rua Marechal Tito', 78, 'A'),
+(8, 'Débora Lins', NULL, 'testedebora@gmail.com', 'laranja18', '18745038936', '2002-07-18', '(11)956327901', NULL, NULL, '07506542', 'São Paulo', 'São Paulo', 'Guaianases', 'Rua Nazaré', 67, NULL),
+(9, 'Rosana Almeida', 'Rosana', 'testerosana@gmail.com', 'margarida', '35926782691', '1997-06-20', '(11)965321765', NULL, NULL, '78954637', 'São Paulo', 'São Paulo', 'Itaim Paulista', 'Rua Morcego cego', 34, 'C'),
+(10, 'Rafael Santana', 'Rafa', 'testerafa@gmail.com', 'jujuba', '26754983672', '1990-03-17', '(11)967543861', NULL, NULL, '07563458', 'São Paulo', 'São Paulo', 'Jardim Lua', 'Rua João Barreto', 77, NULL);
 
 --
